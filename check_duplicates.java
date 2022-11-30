b932a6d2e02cc350726d27ebbc45ce35297103b4
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2022-11-30 20:35:00 +0530

    Add files via upload
    return the duplicate element from the array

---
+++ check_duplicates.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class sum_all {
+    static String[] convert_add(int len){
+        String array[]=new String[len];
+        System.out.println("Enter the array elements : ");
+        for (int i=0;i<len;i++){
+            Scanner get_obj=new Scanner(System.in);
+            array[i] = get_obj.next();
+        }
+        return array;
+    }
+    static String check_duplicates(String[] arr,int lent){
+        //String otp="No duplicates";
+        for (int i=0;i<lent;i++){
+            for (int j=i+1;j<lent;j++){
+                if (arr[i].equals(arr[j])){
+                    return arr[i];}
+        }
+    } return "No duplicates";
+    }
+    public static void main(String[] args){
+        Scanner scan_obj=new Scanner(System.in);
+        System.out.println("Enter the array length : ");
+        int lengt=scan_obj.nextInt();
+        String[] output=convert_add(lengt);
+        String val=check_duplicates(output,output.length);
+        System.out.println(val);
+        }
+    }
